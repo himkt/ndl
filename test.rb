4a96550c5ebeba9bcb7281772317548b0a37ed38
Makoto Hiramatsu <s1311536@u.tsukuba.ac.jp>
2015-02-27 02:48:42 +0900

    update test.rb

--- test.rb
+++ test.rb
@@ -1,9 +1,17 @@
+require 'rexml/document'
 require 'ndl'
 
 open_search = Ndl::OpenSearch.new
 
+# you can see result by using the following code
 result = open_search.search(:title=>'a', :from=>'1111-11-11', :cnt=>2)
-
 while line = result.gets
   puts line
 end
+
+=begin
+# you also can parse 'result' by using xml parser
+result = open_search.search(:title=>'a', :from=>'1111-11-11', :cnt=>2)
+doc = REXML::Document.new(result)
+puts doc
+=end
